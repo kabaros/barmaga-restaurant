dbef3ef3bbbe92a465eb371da29d6dfec0457414
mozafar <kabaros@gmail.com>
2021-02-18 14:44:44 +0000

    Implement form state

--- src/components/AddReservation.tsx
+++ src/components/AddReservation.tsx
@@ -1,16 +1,35 @@
-import React from 'react';
+import React, { useState } from 'react';
 import LabelWithInput from './LabelWithInput';
 
-// DRY - Do not Repeat Yourself
+
+type ReservationType = {
+  name: string;
+  email: string;
+  number: number;
+};
 
 const AddReservation = () => {
+  const [form, setForm] = useState<Partial<ReservationType>>({})
+
+  const onChange = (name: string, value: string) => {
+    setForm({
+      ...form,
+      [name]: value
+    })
+  };
+
+  const onSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    alert(JSON.stringify(form));
+  }
+
   return (
     <div>
       <h1>اضافة حجز</h1>
-      <form>
-        <LabelWithInput name="name" placeholder="الاسم"/>
-        <LabelWithInput name="email" placeholder="الايميل"/>
-        <LabelWithInput name="number" placeholder="العدد" type="number"/>
+      <form onSubmit={onSubmit}>
+        <LabelWithInput onChange={onChange} name="name" placeholder="الاسم"/>
+        <LabelWithInput onChange={onChange} name="email" placeholder="الايميل"/>
+        <LabelWithInput onChange={onChange} name="number" placeholder="العدد" type="number"/>
         <div>
           <input type="submit" value="اضافة حجز"/>
         </div>
--- src/components/LabelWithInput.tsx
+++ src/components/LabelWithInput.tsx
@@ -3,15 +3,20 @@ import React from 'react';
 type LabelWithInputProps =  {
     name: string;
     placeholder: string;
+    onChange: (name: string, value: string) => void;
     type?: string;
 };
-  
+
 const LabelWithInput: React.FC<LabelWithInputProps> = (props) => {
-  const { name, placeholder, type = "text" } = props;
+  const { name, placeholder, type = "text", onChange } = props;
+
+  const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    onChange(event.target.name, event.target.value);
+  };
 
   return (<div>
     <label htmlFor={name}>{placeholder}</label>
-    <input type={type} placeholder={placeholder} name={name}></input>
+    <input onChange={handleChange} type={type} placeholder={placeholder} name={name}></input>
   </div>);
 };
 
