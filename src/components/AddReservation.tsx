7612d7eef352e17d640eded8d56bcbec0a63b1b7
mozafar <kabaros@gmail.com>
2021-02-18 07:26:52 +0000

    Create first components

---
+++ src/components/AddReservation.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const AddReservation = () => {
+  return (
+    <div>
+      <h1>اضافة حجز</h1>
+    </div>
+  );
+};
+
+export default AddReservation;
---
+++ src/components/ReservationsList.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const ReservationsList = () => {
+  return (
+    <div>
+      <h2>قائمة الحجز</h2>
+    </div>
+  );
+};
+
+export default ReservationsList;
